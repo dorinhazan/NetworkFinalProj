d073067537ab8ea81be5c3ee57a7a4864bb3cc1c
yuvtz <tzuryuval@gmail.com>
2024-04-19 12:35:37 +0300

    test

---
+++ BotClient.py
@@ -0,0 +1,59 @@
+import random
+import select
+import time
+from ClientMain import ClientMain
+
+
+
+class BotClient(ClientMain):
+    def __init__(self):
+        super().__init__()
+
+    # Overriding the game_mode method
+    def game_mode(self):
+        """Enters game mode - automatically answers questions."""
+        try:
+            game_over_received = False
+            while not game_over_received:
+                readable, _, _ = select.select([self.tcp_socket], [], [], None)
+                if self.tcp_socket in readable:
+                    message = self.tcp_socket.recv(1024).decode().strip()
+                    if message:
+                        print(message)
+                        if "Game over!" in message:
+                            game_over_received = True
+                            break
+                        if "True or false" in message:
+                            # Start a timer for 10 seconds to wait for an answer
+                            print(f'bot:{self.name}')
+
+                            start_time = time.time()
+                            while (time.time() - start_time) < 10:
+                                time.sleep(4)
+                                answer = random.choice(['Y', '1', 'T', 'N', '0', 'F'])
+                                self.tcp_socket.sendall(answer.encode())
+                                break
+        except Exception as e:
+            print(f"[Bot {self.name}] An error occurred: {e}")
+        finally:
+            print(f"Server disconnected, listening for offer requests...")
+            self.tcp_socket.close()
+            self.listen_for_udp_broadcast()
+
+    def run(self):
+        while True:
+            try:
+                self.name = "Bot:" + random.choice(self.player_names)
+                self.listen_for_udp_broadcast()
+                self.connect_to_server()
+                self.game_mode()
+            except Exception as e:
+                print(f"Error encountered: {e}. Attempting to reconnect...")
+                if self.tcp_socket:
+                    self.tcp_socket.close()  # Ensure the socket is closed before retrying
+
+
+if __name__ == "__main__":
+    bot = BotClient()
+    bot.run()
+
